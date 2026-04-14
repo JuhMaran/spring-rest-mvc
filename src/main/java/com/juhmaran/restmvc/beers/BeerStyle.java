a34585e05bbf2c4caa497b085e5f07b8d75db2fb
JuhMaran <julianemaran@gmail.com>
2026-04-14 09:21:52 -0300

    Adding models

---
+++ src/main/java/com/juhmaran/restmvc/beers/BeerStyle.java
@@ -0,0 +1,11 @@
+package com.juhmaran.restmvc.beers;
+
+/**
+ * spring-rest-mvc
+ *
+ * @author Juliane Maran
+ * @since 14/04/2026
+ */
+public enum BeerStyle {
+  LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ src/main/java/com/juhmaran/restmvc/customers/Customer.java
@@ -0,0 +1,27 @@
+package com.juhmaran.restmvc.customers;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * spring-rest-mvc
+ *
+ * @author Juliane Maran
+ * @since 14/04/2026
+ */
+@Data
+@Builder
+public class Customer {
+
+  private UUID id;
+  private Integer version;
+
+  private String name;
+
+  private LocalDateTime createdDate;
+  private LocalDateTime updateDate;
+
+}
